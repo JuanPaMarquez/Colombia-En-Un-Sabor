f58d928f5fdd954afc6cd81f7a8ab39c4c743a76
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-06-10 19:38:56 -0500

    Mejora el diseño de la navegación: ajusta la alineación y el tamaño del ícono.

--- src/ui/Inicio/Navegacion.tsx
+++ src/ui/Inicio/Navegacion.tsx
@@ -1,8 +1,8 @@
 export default function Navegacion() {
   return (
     <nav className="text-center bg-[#FCE4C2] py-2">
-      <div className="flex justify-center items-center">
-        <img className="size-15" src="./icono.png" alt="" />
+      <div className="flex justify-center items-center flex-col md:flex-row">
+        <img className="size-20" src="./icono.png" alt="" />
         <h1 className="text-4xl font-bold">Colombia En Un Sabor</h1>
       </div>
       <h2 className="text-2xl cursive text-gray-800">"Un sabor de tradición, una cucharada de innovación"</h2>
